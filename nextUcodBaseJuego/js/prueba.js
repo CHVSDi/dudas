0f0cb677aa7cbdbd86a75f3d93faeb47bc799c3d
Unknown <chavho88@hotmail.com>
2018-05-01 10:54:17 -0600

    nuevo problema ahora en la parte de eliminar los elementos que formen una linea de tres o mas con un id identico

--- nextUcodBaseJuego/js/prueba.js
+++ nextUcodBaseJuego/js/prueba.js
@@ -1,97 +1,133 @@
-$(document).ready(function() {
-  function AddCandies(col) {
-    var image = [
-      "<div class='parent'><img src='image/1.png' class='child' id='candy1' style='width: 90px; margin:4px'></div>",
-      "<div class='parent'><img src='image/2.png' class='child' id='candy2' style='width: 90px; margin:4px'></div>",
-      "<div class='parent'><img src='image/3.png' class='child' id='candy3' style='width: 90px; margin:4px'></div>",
-      "<div class='parent'><img src='image/4.png' class='child' id='candy4' style='width: 90px; margin:4px'></div>"
-    ]
-    for (var i = 0; i < 7; i++) {
-      var x = Math.floor(Math.random()*4);
-      $(col).append(image[x])
-    }
-    var chld = $(col).css("position","relative");
-    candyFall();
-    function candyFall(){
-      chld.css("bottom","300px");
-      chld.animate({bottom: -1}, "slow");
+$(function() {
+  //al pulsar el botón de reinicio en este caso inicio:
+    $(".btn-reinicio").click(function(){
 
-    }
-  }
+      //1. cambia el texto de inicio a reiniciar
+      var $this = $(this);
+      $this.toggleClass(".btn-reinicio");
+      if($this.hasClass(".btn-reinicio")){
+        $this.text("Reiniciar");
+      }
+      else {
+        $this.text("Iniciar")
+      }
 
-  AddCandies(".col-1");
 
-  checkedCandy = {
-    init : function() {
-      var candylist = $(".col-1").toArray();
-      for (var a = 0; a < candylist.length; a++) {
-        for (var b = a+1; b < candlist.length; b++) {
-          for (var c = b+1; c < candylist.length; c++) {
-            if (candylist[b]==candylist[c]) {
-              var candy = candylist[b];
-            }
-          }
+      function addCandies(col) {
+        var image = [
+          "<img src='image/1.png' class='child' id='candy1' style='width: 80px; margin:6px'>",
+          "<img src='image/2.png' class='child' id='candy2' style='width: 80px; margin:6px'>",
+          "<img src='image/3.png' class='child' id='candy3' style='width: 80px; margin:6px'>",
+          "<img src='image/4.png' class='child' id='candy4' style='width: 80px; margin:5px'>"
+        ]
+        for (var i = 0; i < 7; i++) {
+          var x = Math.floor(Math.random()*4);
+          $(col).append("<div class='parent'>"+image[x]+"</div>");
+        }
+        var chld = $(col).css("position","relative");
+        candyFall();
+        function candyFall(){
+          chld.css("bottom","90px");
+          chld.animate({bottom: -1}, "slow");
+
         }
       }
-      var candyId = $(candy).attr("id");
-      return candyId;
-    }
-  }
+      //2. Se añaden los dulces en cada columna
+      addCandies(".col-1");
+      addCandies(".col-2");
+      addCandies(".col-3");
+      addCandies(".col-4");
+      addCandies(".col-5");
+      addCandies(".col-6");
+      addCandies(".col-7");
+
+      /*3. (el problema lo tengo aquí). se destruyen todos los elementos que por su mismo
+           id crean una linea veritical*/
+      destroyCheckedCandies(".col-1");
+      destroyCheckedCandies(".col-2");
+      destroyCheckedCandies(".col-3");
+      destroyCheckedCandies(".col-4");
+      destroyCheckedCandies(".col-5");
+      destroyCheckedCandies(".col-6");
+      destroyCheckedCandies(".col-7");
+
+    /*4. se le permite al usuario trasladar los dulces dese una posición a otra*/
+    jQuery.fn.swap = function(b){
+      b = jQuery(b)[0];
+      var a = this[0];
+      var t = a.parentNode.insertBefore(document.createTextNode(''), a);
+      b.parentNode.insertBefore(a, b);
+      t.parentNode.insertBefore(b, t);
+      t.parentNode.removeChild(t);
+      return this;
+    };
 
 
-    $(".child").draggable({
-      revert: true
-    });
+  $( ".child" ).draggable({ revert: true, helper: "clone" });
 
-    var draggableCandy = $(this).attr("id")
+  $( ".child" ).droppable({
+      accept: ".child",
+      activeClass: "ui-state-hover",
+      hoverClass: "ui-state-active",
+      drop: function( event, ui ) {
 
-    $(".parent").droppable({
-      if (draggableCandy==checkedCandy) {
-        accept: '.child',
-          drop: function(event, ui) {
-          if ($(this).children().length > 0) {
-            var move = $(this).children().detach();
-            $(ui.draggable).parent().append(move);
-          }
-          $(this).append($(ui.draggable));
-        }
+          var draggable = ui.draggable, droppable = $(this),
+              dragPos = draggable.position(), dropPos = droppable.position();
+
+          draggable.css({
+              left: dropPos.left+'px',
+              top: dropPos.top+'px'
+          });
+
+          droppable.css({
+              left: dragPos.left+'px',
+              top: dragPos.top+'px'
+          });
+          draggable.swap(droppable);
       }
+
     });
-
-    var colLn= $(".col-1").toArray;
-    var colVct = [
-      "$('.col-1').chidren('#candy1')",
-      "$('.col-1').chidren('#candy2')",
-      "$('.col-1').children('#candy3')",
-      "$('.col-1').children('#candy4')"
-    ];
-
-    for (var a = 0; a < colLn.length; a++) {
-      for (var b = a+1; b < colLn.length; b++) {
-        for (var c = b+1; c < colLn.length; c++) {
-          if (colLn[a]==colVct[b] && col[b]==colVct[c]) {
-            for (var d = c+1; d < colLn.length; d++) {
-              if (colLn[c]==colVct[d]) {
-                $(col[a]).hide();
-                $(col[b]).hide();
-                $(col[c]).hide();
-                $(col[d]).hide();
+    /*Aquí se encuentra el problema, no se como hacer para que cada vez que se encuentren tres o más dulces del mismmo tipo(id)
+    estos se eliminen este es al que recurrí de ultimo pero no sé porqué este no funciona, no da ningun error solamente no hace lo}
+    que quiero hacer*/
+    function destroyCheckedCandies(col) {
+      var column= $(col).toArray();
+      var idCol = [
+        "column[0].attr('id')",
+        "column[1].attr('id')",
+        "column[2].attr('id')",
+        "column[3].attr('id')",
+        "column[4].attr('id')",
+        "column[5].attr('id')",
+        "column[6].attr('id')"
+      ]
+      for (var a = 0; a < col.length; a++) {
+        for (var b = a+1; b < col.length; b++) {
+          for (var c = b+1; c < col.length; c++) {
+            if (idCol[a]==idCol[b] && idCol[b]==idCol[c]) {
+              for (var d = c+1; d < col.length; d++) {
+                if (idCol[c]==idCol[d]) {
+                  $(column[a]).remove();
+                  $(column[b]).remove();
+                  $(column[c]).remove();
+                  $(column[d]).remove();
+                  break;
+                }
                 break;
               }
+              $(column[a]).remove();
+              $(column[b]).remove();
+              $(column[c]).remove();
               break;
             }
-            $(col[a]).hide();
-            $(col[b]).hide();
-            $(col[c]).hide();
             break;
           }
-          break;
+          continue;
         }
-        break;
       }
 
     }
 
 
-
+  });
 });
